bc307b42dac560c7b5e230f8d66db791c2b3070e
Anders Johansen <anders.joh@gmail.com>
2018-11-16 13:05:42 +0100

    Getting closer

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,15 +1,14 @@
 object SumOfMultiples {
-  val isMultiple = List[Int], Int => (factors: List[Int], n: Int) : Boolean => { 
-      factors match {
+  val isMultiple : (List[Int], Int) => Boolean = (factors, n)  => factors match {
         case Nil => false
         case head :: tail => (n % head == 0) || isMultiple(tail, n) 
     }
-  }
+  
   def sum(factors: Set[Int], limit: Int): Int = { 
-    val r = (0 until limit)  
+    val r = 0 until limit 
     
     val multiples = r match {
-      case i if isMultiple(factors, i) => i
+      case i if isMultiple(factors.toList, i) => i
     }  
     sum(multiples)
   }
